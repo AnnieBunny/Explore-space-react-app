ac2940a4be83648451124402409a749bd47ae39d
Annie <75939205+AnnieBunny@users.noreply.github.com>
2021-12-04 15:24:36 +0200

    added details page

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,15 +1,47 @@
 
+import styles from './Details.module.css';
 
 const Details = () => {
 
     return (
 
-        <section>
-            <article>
+        <section className={styles['details-container']}>
+            <article className={styles['details-post']}>
+                <p className={styles['details-post-title']}>Definition and Usage</p>
+                <p className={styles['details-post-text']}>
+                    he overflow property specifies what should happen if content overflows an element's box.
+                    This property specifies whether to clip content or to add scrollbars when an element's content is too big to fit in a specified area.
+                    Note: The overflow property only works for block elements with a specified height.
+                    Default value:	visible
+                    Inherited:	no
+                    Animatable:	no. Read about animatable
+                    Version:	CSS2
+                    JavaScript syntax:	object.style.overflow="scroll"
+                    he overflow property specifies what should happen if content overflows an element's box.
+                    This property specifies whether to clip content or to add scrollbars when an element's content is too big to fit in a specified area.
+                    Note: The overflow property only works for block elements with a specified height.
+                    Default value:	visible
+                    Inherited:	no
+                    Animatable:	no. Read about animatable
+                    Version:	CSS2
+                    JavaScript syntax:	object.style.overflow="scroll"
+
+                </p>
 
             </article>
-            <article>
+            <article className={styles['details-post-ownerInfo']} >
+                <p className={styles['details-post-username']}><strong>Authour: </strong> ani@abv.bg</p>
+                <p className={styles['details-post-created']}><strong>Date: </strong>12.12.1212</p>
+                <p className={styles['details-post-likes']}>Likes: 8</p>
 
+            </article>
+
+            <article className={styles['details-post-btn-container']}>
+                <button className={styles['edit-btn']}>Edit</button>
+                <button className={styles['delete-btn']}>Delete</button>
+
+                <button className={styles['like-btn']}>Like</button>
+                <button className={styles['back-btn']}>Back</button>
             </article>
         </section>
     )
--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3030/data';
+const url = 'http://localhost:3030/jsonstore';
 
 
 export const getAllPosts = async () => {
